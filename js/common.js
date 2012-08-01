bbfecec6f0b304a73cf3a77f985f950e6abce04f
Leeke <Leeke@Leeke-PC>
2012-08-01 14:05:49 +0800

    Signed-off-by: Leeke <Leeke@Leeke-PC>

--- js/common.js
+++ js/common.js
@@ -7,20 +7,58 @@
 	    }    
 	}
 	
-	if(f_chrome==1){	
-		window.onload = function() {		
-			document.body.onkeydown = function(e) {
-				e = e || window.event;
-				// 把键值转换成字母
-				var key = String.fromCharCode(e.keyCode); 
-				var dom = document.getElementById(key);
-				if(document.getElementById(key).style.display=="block" || document.getElementById(key).style.display=="inline-block"){
-					if(dom) {
-						dom.click();
-					}
-				}else{
-					//alert("error");	
-				}
+	function getValue(str){
+		var url = location.href;
+		var theRequest = new Object();
+		var paraString = url.substring(url.indexOf("?") + 1, url.length).split("&");
+		if (url.indexOf("?") != -1) {
+			for (var i = 0; i < paraString.length; i++) {
+				theRequest[paraString[i].split("=")[0]] = unescape(paraString[i].split("=")[1]);
+			}
+		}
+		
+		x=theRequest[str];
+		
+		return x;
+	}
+
+	function usual_search(data,key){
+		var m=data.length;
+		for(i=0;i<m;i++){
+			if(data[i].ID==key){
+				return i
 			}
 		}
 	}
+	
+	function usual_search2(data,key){
+		var m=data.length;
+		for(i=0;i<m;i++){
+			if(data[i].id==key){
+				return i
+			}
+		}
+	}
+	
+	function printSize(data, pid, callback){
+		var comments=[];
+		for(var i=0, len=newsJSON.length; i<len; i++){
+			if(newsJSON[i].categoryId==pid){
+				comments.push(newsJSON[i]);
+			}	
+		}
+		
+		var xx = comments.length;
+		if(callback) callback(xx);
+	}
+	
+	function f_control_num2(num,comments,testSize){
+	if(num>0){
+		var comments2 = [];
+		for(i=0;i<=testSize-1;i++){
+			comments2.push(comments[i]);
+		}
+		comments = comments2;
+	}
+	return comments;
+}
